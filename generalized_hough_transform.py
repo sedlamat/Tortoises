d710ab598dd7b753de74ff28f7132d5a49372362
Matej Sedlacek <sedlamat@gmail.com>
2016-05-10 15:59:50 +0200

    save

--- generalized_hough_transform.py
+++ generalized_hough_transform.py
@@ -1,4 +1,3 @@
-
 import os
 import math
 import numpy as np
@@ -41,12 +40,14 @@ def get_edges_combo(img):
                                   min_of_BGR))
     display_img(color_only)
     mag_color_only = get_gradient_magnitude(color_only)
+    display_img(mag_color_only)
     mag_min_of_BGR = get_gradient_magnitude(min_of_BGR)
     mag = mag_color_only*mag_min_of_BGR
     max_val = np.max(mag)
     koef = max_val/255.0
     edges_mag = cv2.threshold(mag,25000,255,cv2.THRESH_BINARY)[1]
     edges_mag = edges_mag.astype(np.uint8)
+    display_img(edges_mag)
     edges = cv2.Canny(min_of_BGR,0,50)
     edges = cv2.bitwise_and(edges,edges_mag)
     return edges
@@ -78,21 +79,33 @@ def get_r_table(template_orientations,reference_point):
     return r_table
                     
 def get_accumulator(r_table,img_orientations):
-    #display_img(img_orientations)
-    range_val = (1,45,90,135)
+    '''Generating Hough accumulator.
+    '''
+    quant_values = (1,45,90,135)
+    assert len(quant_values)==len(r_table)
+    quant_positions = [[tuple(pos) for pos in np.argwhere(img_orientations==value)]
+                 for value in quant_values]
     accumulator = np.zeros(img_orientations.shape)
-    #print accumulator.dtype
-    assert len(range_val)==len(r_table)
-    for quant_idx, quant_val_diffs in enumerate(r_table):
-        print quant_idx,
-        #print len(quant_val_diffs)
-        orient_positions = (img_orientations==range_val[quant_idx])
-        positions = [tuple(pos) for pos in np.argwhere(orient_positions)]
-        #print len(positions)
-        #print len(positions)/100*0.05*4/60
+    print('For 1 r_table orientation (out of 4):')
+    print('Numbers for the 4 quants (on img and template):')
+    total_num_iter = 0
+    for quant_idx in xrange(len(quant_values)):
+        print(len(quant_positions[quant_idx]),
+              len(r_table[quant_idx]))
+        total_num_iter += len(quant_positions[quant_idx])*len(r_table[quant_idx])
+    print('Total number of operations per r_table:',
+          total_num_iter)
+    print('Total number of operations per tortoise:',
+          total_num_iter*4)
+    num_accu_iter = 0
+    total_time_accu_iter = 0.0
+    for quant_idx in xrange(len(quant_values)):
+        positions = quant_positions[quant_idx]
+        quant_val_diffs = r_table[quant_idx]
         for y,x in positions:
-            t0 = time.time()
             for y_diff, x_diff in quant_val_diffs:
+                num_accu_iter += 1
+                t0 = time.time()
                 yr = y_diff + y
                 xr = x_diff + x
                 a = None
@@ -130,10 +143,11 @@ def get_accumulator(r_table,img_orientations):
                                 accumulator[yy,xx_f] += 1
                             if xx_c >= 0 and xx_c < accumulator.shape[1]:
                                 accumulator[yy,xx_c] += 1
-            #print((time.time()-t0))
-            #print np.sum(img_orientations>0)
-            #print img_orientations.size
-        #print quant_idx
+                total_time_accu_iter += time.time()-t0
+    time_accu_iter = total_time_accu_iter/num_accu_iter
+    print('One accu iter takes:',time_accu_iter,'s')
+    print('Total time of tortoise processing:',
+          time_accu_iter*total_num_iter*4,'s')
     return accumulator
 
 def get_edges_orientations(img, img_edges):
@@ -165,37 +179,41 @@ def get_rotated_r_tables(r_table,rotations):
 def main():
     
     directory_ght = os.path.expanduser('~') + r'/Images/Generalized_Hough_Transform/'
+    ght_template_file_name = directory_ght + 'ght_template.bmp'
+    ght_template_edges_file_name = directory_ght + 'ght_template_edges.bmp'
+    ght_template = cv2.imread(ght_template_file_name)
+    ght_template_edges = cv2.imread(ght_template_edges_file_name)
+    ght_template = ght_template[:,:,0]
+    ght_template_edges = ght_template_edges[:,:,0]
+    ref_point = tuple(np.argwhere(ght_template_edges == 127)[0])
+    ght_template_edges = cv2.threshold(ght_template_edges,
+                                       200,
+                                       255,
+                                       cv2.THRESH_BINARY)[1]
+    template_edges_orientations = get_edges_orientations(ght_template,
+                                                         ght_template_edges)
+    r_table = get_r_table(template_edges_orientations,
+                          ref_point)
+    print len(r_table[0]),len(r_table[1]),len(r_table[2]),len(r_table[3])
+    rotations = [angle for angle in xrange(0,360,90)]
+    rotated_r_tables = get_rotated_r_tables(r_table,rotations)
+    
     directory_images = os.path.expanduser('~') + r'/Images/Tortoises/'
     tortoise_images = os.listdir(directory_images)
-    print tortoise_images, len(tortoise_images)
+    #print tortoise_images, len(tortoise_images)
     for img_name in tortoise_images[:1]:
-        img_file_name = directory_images + img_name
-        ght_template_file_name = directory_ght + 'ght_template.bmp'
-        ght_template_edges_file_name = directory_ght + 'ght_template_edges.bmp'
+        img_name = 'tortoise.jpg'
+        img_file_name = directory_ght    + img_name
         img = cv2.imread(img_file_name)
-        ght_template = cv2.imread(ght_template_file_name)
-        ght_template_edges = cv2.imread(ght_template_edges_file_name)
+        
         resize_koef = 256.0/max(img.shape)
         img = cv2.resize(img,(0,0),fx=resize_koef,fy=resize_koef)
         img_edges = get_edges_combo(img)
-        
-        ght_template = ght_template[:,:,0]
-        ght_template_edges = ght_template_edges[:,:,0]
-        ref_point = tuple(np.argwhere(ght_template_edges == 127)[0])
-        ght_template_edges = cv2.threshold(ght_template_edges,
-                                           200,
-                                           255,
-                                           cv2.THRESH_BINARY)[1]
-        template_edges_orientations = get_edges_orientations(ght_template,
-                                                             ght_template_edges)
-        img_edges_orientations = get_edges_orientations(img,img_edges)
-        
-        r_table = get_r_table(template_edges_orientations,
-                              ref_point)
-        
-        rotations = [angle for angle in xrange(0,360,45)]
-        print rotations
-        rotated_r_tables = get_rotated_r_tables(r_table,rotations)
+        img_edges_orientations = get_edges_orientations(img,img_edges)        
+        display_img(img)
+        display_img(img_edges)
+        display_img(img_edges_orientations)
+        print np.sum(img_edges_orientations>0)
         accu_results = []
         for rotation_idx, angle in enumerate(rotations):
             print angle
@@ -206,7 +224,6 @@ def main():
             kernel_size = 17
             kernel = np.ones((kernel_size,kernel_size))/(kernel_size*kernel_size)
             accumulator_mask = cv2.threshold(accumulator,1,1,cv2.THRESH_BINARY)[1]
-            display_img(accumulator_mask)
             accumulator = cv2.filter2D(accumulator,cv2.CV_64F,kernel)
             accumulator_mask = cv2.filter2D(accumulator_mask,cv2.CV_64F,kernel)
             accumulator *= accumulator_mask
@@ -216,10 +233,17 @@ def main():
         print accu_results
         accu_results = sorted(accu_results,reverse=True)
         print accu_results
-        y_max = accu_results[0][2][1]
-        x_max = accu_results[0][2][1]
-        print 'angle', angle
-        img[y_max-5:y_max+5,x_max-5:x_max+5] = (0,0,255)
+        for idx,res in enumerate(accu_results):
+            res_angle = res[1]
+            y_max = res[2][1]
+            x_max = res[2][0]
+            color = 255 - 50*idx
+            img[y_max-5:y_max+5,x_max-5:x_max+5] = (0,0,color)
+            print res_angle
+            display_img(img)
+        angle = accu_results[0][1]
+        M = cv2.getRotationMatrix2D((img.shape[1]/2,img.shape[0]/2),angle,1)
+        img = cv2.warpAffine(img,M,(img.shape[1],img.shape[0]))
         display_img(img)
 
 if __name__ == '__main__':
