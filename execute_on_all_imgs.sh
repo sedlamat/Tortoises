922d85c4c793a1b10764812ee8726d2f7a357ab0
Matej Sedlacek <sedlamat@gmail.com>
2016-06-13 17:08:27 +0200

    save

--- execute_on_all_imgs.sh
+++ execute_on_all_imgs.sh
@@ -1,6 +1,7 @@
 
 img_dir_in=$HOME"/Images/Tortoises/"
 img_dir_out=$img_dir_in"Results/"
+img_dir_check=$img_dir_out"GOOD/"
 
 if [ ! -d $img_dir_out ]
 then
@@ -8,19 +9,19 @@ then
 fi
 
 imgs=$(ls -r $img_dir_in | grep ".jpg")
-echo $imgs
+#echo $imgs
 for img_in in $imgs
 do
 	echo "Processing image $img_in"
-	img_out=resZZ${img_in:0:7}".jpg"
+	img_out=res${img_in:0:7}".jpg"
 
-	img_in=$img_dir_in$img_in
-	img_out=$img_dir_out$img_out
-	#echo $img_dir_out$img_out
+	img_in_path=$img_dir_in$img_in
+	img_out_path=$img_dir_out$img_out
+	img_check_path=$img_dir_check$img_out
 
-	if [ ! -f $img_out ]
+	if [ ! -f $img_check_path ]
 	then
-		./tortoise $img_in $img_out
+		./tortoise $img_in_path $img_out_path
 	fi
-	
+
 done
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <limits>
 #include <cmath>
 #include <iomanip>
+#include <ctime>
 
 /* THIRD PARTY LIBRARIES */
 #include <pwd.h>
@@ -18,15 +19,15 @@
 int main(int argc, char *argv[])
 {
     try {
-	passwd *pw = getpwuid(getuid());
-	std::string path(pw->pw_dir);
+	//~ time_t t0 = clock();
+	//~ passwd *pw = getpwuid(getuid());
+	//~ std::string path(pw->pw_dir);
 
-	std::string file_name = "Tg33200.jpg";
-	std::string imgs_path = path + "/Images/Tortoises/";
-	std::string img_path = imgs_path + file_name;
-	//std::string img_path = argv[1];
+	//~ std::string file_name = "Tg33200.jpg";
+	//~ std::string imgs_path = path + "/Images/Tortoises/";
+	//~ std::string img_path = imgs_path + file_name;
+	std::string img_path = argv[1];
 	cv::Mat plastron_img = cv::imread(img_path,1);
-
 
 
 	std::string templ_path = "plastron_template.bmp";
@@ -35,31 +36,33 @@ int main(int argc, char *argv[])
 	cv::Point reference_point(65,125);
 
 
-	std::map<std::string, cv::Point> map_template_junctions;
-	map_template_junctions["1"] = cv::Point(64,58);
-	map_template_junctions["2"] = cv::Point(64,74);
-	map_template_junctions["3"] = cv::Point(64,97);
-	map_template_junctions["4"] = cv::Point(64,105);
-	map_template_junctions["5"] = cv::Point(64,155);
-	map_template_junctions["6"] = cv::Point(64,168);
-	map_template_junctions["7"] = cv::Point(64,185);
+	//~ std::map<std::string, cv::Point> map_template_junctions;
+	//~ map_template_junctions["1"] = cv::Point(64,58);
+	//~ map_template_junctions["2"] = cv::Point(64,74);
+	//~ map_template_junctions["3"] = cv::Point(64,97);
+	//~ map_template_junctions["4"] = cv::Point(64,105);
+	//~ map_template_junctions["5"] = cv::Point(64,155);
+	//~ map_template_junctions["6"] = cv::Point(64,168);
+	//~ map_template_junctions["7"] = cv::Point(64,185);
 	//sedlamat::display(img);
 	sedlamat::GeneralHough general_hough(plastron_img,
 					     plastron_template,
 					     reference_point,
-					     4, 25, 300, 1.0, 0.3, 0,
-					     map_template_junctions);
+					     12, 25, 200, 1.0, 0.3, 0);
+					     //map_template_junctions);
 	//sedlamat::display(img);
 	//sedlamat::print("running");
-	sedlamat::display(general_hough.get_src_edges());
+	//sedlamat::display(general_hough.get_src_edges());
 	general_hough.detect();
-	sedlamat::display(general_hough.get_result_img());
+	//~ time_t t1 = clock();
+	//~ std::cout << (t1-t0)*1.0/CLOCKS_PER_SEC << std::endl;
+	//~ sedlamat::display(general_hough.get_result_img());
 	//~ sedlamat::display(general_hough.get_template_edges());
 	//~ sedlamat::display(general_hough.get_src_edges());
 	//~ sedlamat::display(general_hough.get_src_img());
 	//~ sedlamat::display(general_hough.get_template_img());
 	//~ sedlamat::print("hotovo");
-	//cv::imwrite(argv[2],general_hough.get_result_img());
+	cv::imwrite(argv[2],general_hough.get_result_img());
     } catch (std::string e) {
 	std::cout << "Error when processing " << argv[1] << std::endl;
 	std::cout << " " << e << std::endl;
